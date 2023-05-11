7e60d5c828f4c5a8a827d847d890daf41f865419
Radim Hruška <radim-hruska@outlook.com>
2023-05-11 21:19:19 +0200

    database update

--- carsshop.sql
+++ carsshop.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: May 06, 2023 at 03:07 PM
+-- Generation Time: May 11, 2023 at 07:18 PM
 -- Server version: 5.7.39
--- PHP Version: 7.4.33
+-- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -45,7 +45,8 @@ CREATE TABLE `uctenka` (
   `id` int(11) NOT NULL,
   `iduzivatele` int(11) NOT NULL,
   `datum` date NOT NULL,
-  `vysCena` int(11) NOT NULL
+  `vysCena` int(11) NOT NULL,
+  `adresa` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -70,7 +71,10 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `username`, `password`, `Role`, `Phone`, `Email`) VALUES
 (2, 'honza', '$2y$10$K6xJpbDa53kWmRNxFx3S6eJvN/8uN09mvcw.ZrlrVbst7c1KPFXRC', '2', '', ''),
 (5, 'radim', '$2y$10$hsxxkFmctKeua4DyuR4QeuePUpZ0A4E.8VNFYuJLC6xZDCfWEqTZ2', '2', '', ''),
-(6, 'sdf', '$2y$10$V/zPSpYxbgXmVw70vKWEM.59ZVrwU3Qd/5y27NqvwMOWABPP.HoBy', '1', '', '');
+(6, 'sdf', '$2y$10$V/zPSpYxbgXmVw70vKWEM.59ZVrwU3Qd/5y27NqvwMOWABPP.HoBy', '1', '', ''),
+(7, 'RadimWithParams', '$2y$10$rjMeJfIHyIvMdGH97do2l.YGvAz1ue.SHPkSWTTatfjUKy9aXbsM2', '1', '666666666', 'hruska@hell.com'),
+(8, 'r', '$2y$10$A429mIuOU3wsJHr7g/g7Ze.Dg1hy4TFuHQ9yK5n8TLwVGKu8UIvgG', '1', '333333333', 'dsf'),
+(9, 'a', '$2y$10$jNAFDmPSyQN96pBsVbbpPu3PZBzpIu2QllifLKzd0O62w4FdzvK4u', '2', '666666666', 'dfs');
 
 -- --------------------------------------------------------
 
@@ -81,10 +85,30 @@ INSERT INTO `users` (`id`, `username`, `password`, `Role`, `Phone`, `Email`) VAL
 CREATE TABLE `zbozi` (
   `id` int(11) NOT NULL,
   `nazev` varchar(50) NOT NULL,
-  `typ` varchar(30) NOT NULL,
+  `typ` enum('sportovni','osobni','dodavka','dil','doplnek') NOT NULL,
   `cena` int(11) NOT NULL,
-  `pocet` int(11) NOT NULL
+  `pocet` int(11) NOT NULL,
+  `pic` varchar(50) NOT NULL,
+  `description` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `zbozi`
+--
+
+INSERT INTO `zbozi` (`id`, `nazev`, `typ`, `cena`, `pocet`, `pic`, `description`) VALUES
+(1, 'Auto', 'sportovni', 1000000, 2, '', ''),
+(3, 'dodávka', 'dodavka', 500000, 1, '', ''),
+(4, 'rgb pásek', 'doplnek', 1000, 5, '', ''),
+(9, 'kolo', 'dil', 1000, 2, 'PIC-57A601012DJ-1.jpg', ''),
+(10, 'Nárazník', 'dil', 3000, 10, 'predni-naraznik-bmw-f30-f31-m3-style-11-pdc.jpg', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nunc tincidunt ante vitae massa. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Fusce tellus. Nulla pulvinar eleifend sem. Nullam sit amet magna in magna gravida vehicula. Donec ipsum massa, ullamcorper in, auctor et, scelerisque sed, est. Aliquam erat volutpat. Nullam eget nisl. Maecenas ipsum velit, consectetuer eu lobortis ut, dictum at dui. Nulla turpis magna, cursus sit amet, suscipit a, interdum id, felis. Aliquam erat volutpat. Ut tempus purus at lorem. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Suspendisse nisl. Nulla est. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Integer pellentesque quam vel velit.\r\n\r\nPellentesque ipsum. Phasellus et lorem id felis nonummy placerat. Nullam sit amet magna in magna gravida vehicula. Morbi leo mi, nonummy eget tristique non, rhoncus non leo. Phasellus et lorem id felis nonummy placerat. Maecenas fermentum, sem in pharetra pellentesque, velit turpis volutpat ante, in pharetra metus odio a lectus. Duis condimentum augue id magna semper rutrum. Etiam dui sem, fermentum vitae, sagittis id, malesuada in, quam. Praesent in mauris eu tortor porttitor accumsan. Integer lacinia. Vestibulum erat nulla, ullamcorper nec, rutrum non, nonummy ac, erat. Curabitur sagittis hendrerit ante. Etiam quis quam. Mauris elementum mauris vitae tortor. Sed vel lectus. Donec odio tempus molestie, porttitor ut, iaculis quis, sem. Nulla est.\r\n\r\nMaecenas ipsum velit, consectetuer eu lobortis ut, dictum at dui. Curabitur ligula sapien, pulvinar a vestibulum quis, facilisis vel sapien. Aliquam erat volutpat. Duis viverra diam non justo. Vestibulum erat nulla, ullamcorper nec, rutrum non, nonummy ac, erat. Morbi imperdiet, mauris ac auctor dictum, nisl ligula egestas nulla, et sollicitudin sem purus in lacus. Etiam quis quam. Etiam bibendum elit eget erat. In laoreet, magna id viverra tincidunt, sem odio bibendum justo, vel imperdiet sapien wisi sed libero. Nullam sapien sem, ornare ac, nonummy non, lobortis a enim. Integer imperdiet lectus quis justo. Nulla turpis magna, cursus sit amet, suscipit a, interdum id, felis. Integer imperdiet lectus quis justo. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Duis sapien nunc, commodo et, interdum suscipit, sollicitudin et, dolor.'),
+(12, 'rgb pásek', 'doplnek', 1000, 5, '', ''),
+(15, 'kolo', 'dil', 1000, 2, 'PIC-57A601012DJ-1.jpg', ''),
+(16, 'Stěrač', 'dil', 30, 2, 'sterace-280-mm.jpg', ''),
+(17, 'Stěrač', 'dil', 30, 2, 'sterace-280-mm.jpg', ''),
+(18, 'kolo', 'dil', 1000, 2, 'PIC-57A601012DJ-1.jpg', ''),
+(19, 'kolo', 'dil', 1000, 2, 'PIC-57A601012DJ-1.jpg', ''),
+(20, 'Nárazník', 'dil', 3000, 10, 'predni-naraznik-bmw-f30-f31-m3-style-11-pdc.jpg', '');
 
 --
 -- Indexes for dumped tables
@@ -137,13 +161,13 @@ ALTER TABLE `uctenka`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `zbozi`
 --
 ALTER TABLE `zbozi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- Constraints for dumped tables
